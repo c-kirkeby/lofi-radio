6f6a76ea9e823fb5e3b2dbd936d628294b521a2e
Christian <christian@christiankirkeby.com>
2026-04-25 23:24:26 +1000

    fix: proxy response header handling

--- apps/ui/src/routes/api/proxy/+server.ts
+++ apps/ui/src/routes/api/proxy/+server.ts
@@ -4,7 +4,7 @@ const logger = pino()
 
 import type { RequestHandler } from "@sveltejs/kit";
 
-export const GET: RequestHandler = async ({ url }) => {
+export const GET: RequestHandler = async ({ url, request }) => {
   const target = url.searchParams.get('url')
 
   if (!target) {
@@ -22,15 +22,38 @@ export const GET: RequestHandler = async ({ url }) => {
     return new Response("Invalid URL")
   }
 
-  try {
-    const response = await fetch(targetUrl)
 
-    const headers = new Headers(response.headers);
-    headers.delete('content-encoding');
-    headers.delete('transfer-encoding');
+  const headers = new Headers(request.headers);
+  headers.delete('host');
+  headers.delete('sec-fetch-dest');
+  headers.delete('sec-fetch-mode');
+  headers.delete('sec-fetch-site');
+  headers.set('user-agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
+  headers.set('accept', '*/*');
+  headers.set('accept-language', 'en-US,en;q=0.9');
+  headers.set('referer', new URL(targetUrl).origin);
+  headers.set('connection', 'keep-alive');
+
+  const origin = request.headers.get('origin');
+  try {
+    const response = await fetch(targetUrl, { headers })
+
+    const responseHeaders = new Headers(response.headers);
+
+    responseHeaders.delete('content-encoding');
+    responseHeaders.delete('transfer-encoding');
+    responseHeaders.delete('access-control-allow-headers');
+    responseHeaders.delete('access-control-allow-origin');
+    responseHeaders.delete('access-control-allow-methods');
+    responseHeaders.delete('access-control-allow-credentials');
+    responseHeaders.delete('access-control-max-age');
+    responseHeaders.delete('access-control-expose-headers');
+    responseHeaders.set('access-control-allow-origin', origin ?? '*');
+    responseHeaders.set('access-control-expose-headers', '*');
+    responseHeaders.set('accept-ranges', 'bytes');
 
     return new Response(response.body, {
-      headers
+      headers: responseHeaders,
     });
   } catch (error) {
     logger.error(error)
